8ca66f5d61e318926e08a00f5a9b38ca1e8d0ef3
shashank <shashank.garg@pegasus.ae>
2019-09-18 21:37:51 +0400

    first checkin

---
+++ src/main/java/if_else.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class if_else {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int number = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (number % 2 != 0)
+            System.out.println("Weird");
+        else if (number >= 2 & number <= 5)
+            System.out.println("Not Weird");
+        else if (number >= 6 & number <= 20)
+            System.out.println("Weird");
+        else
+            System.out.println("Not Weird");
+
+    }
+
+}
+
